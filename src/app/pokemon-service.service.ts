cf5f6b08f1163723c2c03ce7e255f4daeaeeb322
Aleksey Petrochenko <markinara@gmail.com>
2024-03-30 13:30:41 +0300

    updated method of functions: fetchPokemonData, goNext, goPrev.

--- src/app/pokemon-service.service.ts
+++ src/app/pokemon-service.service.ts
@@ -1,76 +1,83 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin, Subject, EMPTY } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class PokemonServiceService {
 
-  results: { name: string; url: string; }[] = []
-  rest: { count: number, next: string, prev: string }[] = []
+  // Определение субъекта для оповещения компонентов об изменениях
+  private dataUpdated = new Subject<void>();
+
+  // Метод для оповещения компонентов об изменениях
+  dataUpdated$ = this.dataUpdated.asObservable();
+
+  results: { name: string; url: string; }[] = [];
+  rest: { count: number, next: string, previous: string }[] = [];
   nextButton: string | null = null;
   prevButton: string | null = null;
   myPokemon: { pokeName: string, pokePic: string }[] = [];
   link: string = 'https://pokeapi.co/api/v2/pokemon';
 
-  constructor(private httpClient: HttpClient) {
-    // this.httpClient.get(this.link)
-    //   .subscribe(response => {
-    //     const pokemonResults = response as { results: { name: string, url: string }[] }
-    //     const pokemonRest = response as { count: number, next: string, prev: string }
-    //     this.rest.push(pokemonRest)
-    //     this.results.push(...pokemonResults.results);
-    //     console.log(this.results);
-    //     this.nextButton = pokemonRest.next
-    //     this.prevButton = pokemonRest.prev
+  constructor(private httpClient: HttpClient) {}
 
-    //     for (const pokemon of this.results) {
-    //       this.httpClient.get(pokemon.url)
-    //         .subscribe(response => {
-    //           const pokeData = response as { name: string, sprites: { front_default: string } }
-    //           this.myPokemon.push({ pokeName: pokeData.name, pokePic: pokeData.sprites.front_default })
-    //         });
-    //     }
-    //   });
-  }
-
-  fetchPokemonData(url: string) {
-    this.httpClient.get(url)
-      .subscribe(response => {
-        const pokemonResults = response as { results: { name: string, url: string }[] }
-        const pokemonRest = response as { count: number, next: string, prev: string }
-        this.rest.push(pokemonRest)
-        this.results.push(...pokemonResults.results);
-        this.nextButton = pokemonRest.next
-        this.prevButton = pokemonRest.prev
-
-        for (const pokemon of this.results) {
-          this.httpClient.get(pokemon.url)
-            .subscribe(response => {
-              const pokeData = response as { name: string, sprites: { front_default: string } }
-              this.myPokemon.push({ pokeName: pokeData.name, pokePic: pokeData.sprites.front_default })
-            });
+  fetchPokemonData = (url: string): Observable<{ pokeName: string; pokePic: string; }[]> => {
+    return this.httpClient.get(url).pipe(
+      switchMap((response: any) => {
+        const pokemonResults = response.results as { name: string, url: string }[];
+        const pokemonRest = response as { count: number, next: string, previous: string };
+        if (pokemonRest.previous) {
+          this.prevButton = pokemonRest.previous;
+        } else {
+          this.prevButton = null;
         }
-        console.log(this.myPokemon)
-        return this.myPokemon
-      });
+  
+        this.rest.push(pokemonRest);
+        this.results.push(...pokemonResults);
+        this.nextButton = pokemonRest.next;
+        this.prevButton = pokemonRest.previous;
 
+        console.log(this.prevButton)
+  
+        const requests = pokemonResults.map(pokemon => this.httpClient.get(pokemon.url));
+        return forkJoin(requests).pipe(
+          map((pokeResponses: any[]) => {
+            const myPokemon: { pokeName: string; pokePic: string; }[] = [];
+            pokeResponses.forEach(pokeResponse => {
+              const pokeData = pokeResponse as { name: string, sprites: { front_default: string } };
+              myPokemon.push({ pokeName: pokeData.name, pokePic: pokeData.sprites.front_default });
+            });
+            return myPokemon;
+          })
+        );
+      })
+    );
   }
 
-
-  goNext = () => {
+  goNext = (): Observable<void> => {
     if (this.nextButton) {
-      this.fetchPokemonData(this.nextButton);
+      return this.fetchPokemonData(this.nextButton).pipe(
+        map(() => {
+          this.dataUpdated.next(); // Оповещение компонентов об изменениях
+        })
+      );
     }
+    return new Observable<void>();
   }
 
-  goPrev = () => {
+  goPrev = (): Observable<void> => {
     if (this.prevButton) {
-      this.fetchPokemonData(this.prevButton);
+      console.log('Previous button clicked. Fetching data for previous page from url: ', this.prevButton)
+      return this.fetchPokemonData(this.prevButton).pipe(
+        map(() => {
+          this.dataUpdated.next(); // Оповещение компонентов об изменениях
+        })
+      );
     }
-  }
+    console.log("Previous button clicked but no previous url found.")
+    return EMPTY; // Возвращаем пустой Observable, если нет предыдущей страницы
+  }  
 }
-
 
--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
-import { Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { PokemonServiceService } from '../pokemon-service.service';
 
 @Component({
@@ -10,20 +7,40 @@ import { PokemonServiceService } from '../pokemon-service.service';
   styleUrls: ['./pokemons.component.css']
 })
 
-export class PokemonsComponent {
+export class PokemonsComponent implements OnInit {
 
   pokemons: { pokeName: string; pokePic: string; }[] = [];
+  link: string = 'https://pokeapi.co/api/v2/pokemon';
 
-  constructor(private httpClient: HttpClient, private pokemonService: PokemonServiceService) {
-    pokemonService.fetchPokemonData('https://pokeapi.co/api/v2/pokemon')
+  constructor(private pokemonService: PokemonServiceService) {}
+
+  ngOnInit(): void {
+    this.fetchData(this.link); // Используем начальный URL-адрес при инициализации компонента
   }
 
-   onClickNext = () => {
-      this.pokemonService.goNext() ;
+  fetchData(url: string) {
+    this.pokemonService.fetchPokemonData(url).subscribe(pokemonData => {
+      this.pokemons = pokemonData;
+    });
+  }
+
+  onClickNext() {
+    this.pokemonService.goNext().subscribe(() => {
+      if (this.pokemonService.nextButton !== null) {
+        this.fetchData(this.pokemonService.nextButton!); // Используем переменную nextButton
+      }
+    });
   }
   
-  onClickPrev = () => {
-    this.pokemonService.goPrev();
+  onClickPrev() {
+    console.log("Previous button clicked"); // Отладочный вывод
+    if (this.pokemonService.prevButton !== null) {
+      this.pokemonService.goPrev().subscribe(() => {
+        console.log("Previous data fetched"); // Отладочный вывод
+        this.fetchData(this.pokemonService.prevButton!); // Используем переменную prevButton
+      });
+    }
+  }
+  
+  
 }
-}
-
